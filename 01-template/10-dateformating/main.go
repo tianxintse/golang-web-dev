78b5fd897aa0718901a6899e4f4a8e2702658edc
Tianxin Tse <xietx1995@gmail.com>
2018-09-26 23:25:41 +0800

    study templates

---
+++ 01-template/10-dateformating/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+	"time"
+)
+
+var tpl *template.Template
+
+var fm = template.FuncMap{
+	"formatDateMDY": monthDayYear,
+}
+
+func monthDayYear(t time.Time) string {
+	return t.Format("01-02-2006")
+}
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("test.html"))
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "test.html", time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
